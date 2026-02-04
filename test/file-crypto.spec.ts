9af10ac38b64f39abb8c8ffb29028810d70ca1dc
nolawnchairs <nolawnchairs@gmail.com>
2026-02-04 15:52:41 +0700

    tests: added throw expectations

--- test/file-crypto.spec.ts
+++ test/file-crypto.spec.ts
@@ -7,6 +7,12 @@ describe('FileCrypto', () => {
       const context = FileCrypto.createContext(KEY)
       expect(context).toBeDefined()
     })
+    it('should throw an error if the key is too short', () => {
+      expect(() => FileCrypto.createContext(Buffer.alloc(15))).toThrow()
+    })
+    it('should enforce minKeyBytes', () => {
+      expect(() => FileCrypto.createContext(Buffer.alloc(32), { minKeyBytes: 64 })).toThrow()
+    })
   })
   describe('createChecksumContext', () => {
     it('should create a checksum context', () => {
